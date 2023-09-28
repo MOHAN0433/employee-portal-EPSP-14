572b16878c9c0273616b5d9385f8dccba83d61fe
Mohan Patil <mohan.patil@hyniva.com>
2023-09-28 19:07:21 +0530

    remove undefined fields

--- api.js
+++ api.js
@@ -7,48 +7,51 @@ const createEmployee = async (event) => {
   const response = { statusCode: 200 };
   try {
     const body = JSON.parse(event.body);
-    const bankDetails= body.bankDetails
-    // Check for required fields
-    if (!body.bankDetails.BankName || !body.bankDetails.BranchName || !body.bankDetails.BranchAddress || !body.bankDetails.BankAccountNumber) {
-      throw new Error('Required fields are missing.');
+    const postId = body.postId; // Get postId from the body
+
+    // Check for required fields and validate bank details
+    const { bankDetails } = body;
+    if (!bankDetails || !bankDetails.BankName || !bankDetails.BranchName || !bankDetails.BranchAddress || !bankDetails.BankAccountNumber || typeof bankDetails.IsSalaryAccount !== 'boolean' || typeof bankDetails.IsActive !== 'boolean' || typeof bankDetails.IsDeleted !== 'boolean') {
+      throw new Error('Invalid or missing fields in bankDetails.');
     }
 
-    //const id = body.postId;
+    // Check if the employee already exists
     const empData = {
-        TableName: process.env.DYNAMODB_TABLE_NAME,
-        Key: marshall({ postId: body.postId }),
-      };
-      const { Item } = await db.send(new GetItemCommand(empData));
-      
+      TableName: process.env.DYNAMODB_TABLE_NAME,
+      Key: marshall({ postId }),
+    };
+    const { Item } = await db.send(new GetItemCommand(empData));
     if (Item) {
-throw new Error("already exists")
+      throw new Error("Employee with postId already exists.");
     }
 
+    // Create and save the employee record
     const params = {
       TableName: process.env.DYNAMODB_TABLE_NAME,
       Item: marshall({
-        postId: body.postId,
-    bankDetails : {
-        BankName: bankDetails.BankName,//give bank object and validate it and set it bankname
-        BranchName: bankDetails.BranchName,
-        BranchAddress: bankDetails.BranchAddress,
-        CustomerNumber: bankDetails.CustomerNumber,
-        BankAccountNumber: bankDetails.BankAccountNumber,
-        IsSalaryAccount: bankDetails.IsSalaryAccount, //required boolean
-        IsActive: bankDetails.IsActive, //required boolean
-        IsDeleted: bankDetails.IsDeleted, //required boolean
-      }}, { removeUndefinedValues: true }),  //for remove undefined fields
+        postId,
+        bankDetails: {
+          BankName: bankDetails.BankName,
+          BranchName: bankDetails.BranchName,
+          BranchAddress: bankDetails.BranchAddress,
+          CustomerNumber: bankDetails.CustomerNumber,
+          BankAccountNumber: bankDetails.BankAccountNumber,
+          IsSalaryAccount: bankDetails.IsSalaryAccount,
+          IsActive: bankDetails.IsActive,
+          IsDeleted: bankDetails.IsDeleted,
+        },
+      }),
     };
 
     await db.send(new PutItemCommand(params));
     response.body = JSON.stringify({
-      message: 'Successfully created post.',
+      message: 'Successfully created employee record.',
     });
   } catch (e) {
     console.error(e);
     response.statusCode = 500;
     response.body = JSON.stringify({
-      message: 'Failed to create post.',
+      message: 'Failed to create employee record.',
       errorMsg: e.message,
       errorStack: e.stack,
     });
