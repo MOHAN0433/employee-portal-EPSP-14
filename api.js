e7d78bdbb41a4d60c4ca6e93cbb50af89ec5e2ca
Mohan Patil <mohan.patil@hyniva.com>
2023-10-04 17:42:00 +0530

    validations changes new

--- api.js
+++ api.js
@@ -94,57 +94,40 @@ const updateEmployee = async (event) => {
   const response = { statusCode: 200 };
   try {
     const body = JSON.parse(event.body);
-    const bankDetails= body.bankDetails
-    const validationError = validation(bankDetails);
-    if (validationError) {
-      throw new Error(validationError);
-    }
-    // Check for required fields
-    // if (!body.bankDetails.BankName || !body.bankDetails.BranchName || !body.bankDetails.BranchAddress || !body.bankDetails.BankAccountNumber) {
-    //   throw new Error('Required fields are missing.');
-    // }
-
-    //const id = body.postId;
-    const empData = {
-        TableName: process.env.DYNAMODB_TABLE_NAME,
-        Key: marshall({ postId: event.pathParameters.postId }),
-      };
-      const { Item } = await db.send(new GetItemCommand(empData));
-      if (Item) {
-      const item1 = { item2: Item ? unmarshall(Item) : () => {throw new Error("Employee Id not fount")} };
-      console.log(item1);
-      
-    //   if(item1.item2.bankDetails.BankAccountNumber === bankDetails.BankAccountNumber){
-    //   throw new Error("BankAccountNumber already exists");
-    // }
-}
-
+    const objKeys = Object.keys(body);
     const params = {
       TableName: process.env.DYNAMODB_TABLE_NAME,
-      Item: marshall({
-    bankDetails : {
-        BankName: bankDetails.BankName,//give bank object and validate it and set it bankname
-        BranchName: bankDetails.BranchName,
-        BranchAddress: bankDetails.BranchAddress,
-        CustomerNumber: bankDetails.CustomerNumber,
-        BankAccountNumber: bankDetails.BankAccountNumber,
-        IsSalaryAccount: bankDetails.IsSalaryAccount, //required boolean
-        IsActive: bankDetails.IsActive, //required boolean
-        IsDeleted: bankDetails.IsDeleted, //required boolean
-      }}, { removeUndefinedValues: true }),  //for remove undefined fields
+      Key: marshall({ postId: event.pathParameters.postId }),
+      UpdateExpression: `SET ${objKeys
+        .map((_, index) => `#key${index} = :value${index}`)
+        .join(', ')}`,
+      ExpressionAttributeNames: objKeys.reduce(
+        (acc, key, index) => ({
+          ...acc,
+          [`#key${index}`]: key,
+        }),
+        {}
+      ),
+      ExpressionAttributeValues: marshall(
+        objKeys.reduce(
+          (acc, key, index) => ({
+            ...acc,
+            [`:value${index}`]: body[key],
+          }),
+          {}
+        )
+      ),
     };
-
-    
-
-    await db.send(new UpdateItemCommand(params));
+    const updateResult = await db.send(new UpdateItemCommand(params));
     response.body = JSON.stringify({
-      message: 'Successfully created post.',
+      message: 'Successfully updated post.',
+      updateResult,
     });
   } catch (e) {
     console.error(e);
     response.statusCode = 500;
     response.body = JSON.stringify({
-      message: 'Failed to create post.',
+      message: 'Failed to update post.',
       errorMsg: e.message,
       errorStack: e.stack,
     });
