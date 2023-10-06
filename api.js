f2cc6750ada9c51ff4cd9e4f141a547bfc083e12
Mohan Patil <mohan.patil@hyniva.com>
2023-10-06 17:22:32 +0530

    test cases

--- api.js
+++ api.js
@@ -1,16 +1,12 @@
-// Import necessary modules from AWS SDK for DynamoDB
 const { DynamoDBClient, PutItemCommand, GetItemCommand, UpdateItemCommand } = require("@aws-sdk/client-dynamodb");
 const { marshall, unmarshall } = require("@aws-sdk/util-dynamodb");
 
-// Create a DynamoDB client for the specified AWS region
 const db = new DynamoDBClient({ region: "ap-south-1" });
 
-// Define regular expressions for validation
 const nameRegex = /^[A-Za-z]{3,32}$/;
-const CustomerNumberRegex = /^\d{11,12}$/;
+const CustomerNumberRegex = /^\d{11}$/;
 const BankAccountNumber = /^\d{11,16}$/;
 
-// Validation function for bankDetails object
 const validation = (bankDetails) => {
   if (!nameRegex.test(bankDetails.BankName)) {
     return "BankName should be minimum 3 characters!";
@@ -30,68 +26,61 @@ const validation = (bankDetails) => {
   return null; // Validation passed
 }
 
-// Function to create an employee
 const createEmployee = async (event) => {
   const response = { statusCode: 200 };
   try {
-    // Parse the JSON body from the event
     const body = JSON.parse(event.body);
-    const bankDetails = body.bankDetails;
-    
-    // Perform validation on bankDetails
+    const bankDetails= body.bankDetails
     const validationError = validation(bankDetails);
     if (validationError) {
       throw new Error(validationError);
     }
-
-    // Check for required fields in the body
+  // Check for required fields
     if (!body.bankDetails.BankName || !body.bankDetails.BranchName || !body.bankDetails.BranchAddress || !body.bankDetails.BankAccountNumber) {
       throw new Error('Required fields are missing.');
     }
 
-    // Fetch an item from DynamoDB based on postId
-    const employeeData = {
-      TableName: process.env.DYNAMODB_TABLE_NAME,
-      Key: marshall({ postId: body.postId }),
-    };
-    const { Item } = await db.send(new GetItemCommand(employeeData));
-    
-    // Check if an item with the same postId exists in DynamoDB
-    if (Item) {
+    //const id = body.postId;
+    const empData = {
+        TableName: process.env.DYNAMODB_TABLE_NAME,
+        Key: marshall({ postId: body.postId }),
+      };
+      const { Item } = await db.send(new GetItemCommand(empData));
+      if (Item) {
       const item1 = { item2: Item ? unmarshall(Item) : {} };
       console.log(item1);
-
-      // Check if bankDetails already exist in the fetched item
-      if (item1.item2.bankDetails) {
-        throw new Error("BankDetails already exists!");
-      }
+      
+      if(item1.item2.bankDetails.BankAccountNumber === bankDetails.BankAccountNumber || item1.item2.bankDetails.BankName === bankDetails.BankName){
+      throw new Error("BankDetails already exists!");
     }
+}
+      
 
-    // Define parameters for inserting an item into DynamoDB
     const params = {
       TableName: process.env.DYNAMODB_TABLE_NAME,
       Item: marshall({
         postId: body.postId,
-        bankDetails: {
-          BankName: bankDetails.BankName,
-          BranchName: bankDetails.BranchName,
-          BranchAddress: bankDetails.BranchAddress,
-          CustomerNumber: bankDetails.CustomerNumber,
-          BankAccountNumber: bankDetails.BankAccountNumber,
-          IsSalaryAccount: bankDetails.IsSalaryAccount,
-          IsActive: bankDetails.IsActive,
-          IsDeleted: bankDetails.IsDeleted,
-        },
-      }, { removeUndefinedValues: true }),
+    bankDetails : {
+        BankName: bankDetails.BankName,
+        BranchName: bankDetails.BranchName,
+        BranchAddress: bankDetails.BranchAddress,
+        CustomerNumber: bankDetails.CustomerNumber,
+        BankAccountNumber: bankDetails.BankAccountNumber,
+        IsSalaryAccount: bankDetails.IsSalaryAccount,
+        IsActive: bankDetails.IsActive,
+        IsDeleted: bankDetails.IsDeleted,
+      }}, { removeUndefinedValues: true }),
     };
 
-    // Insert the item into DynamoDB
+    
+
     await db.send(new PutItemCommand(params));
     response.body = JSON.stringify({
       message: 'Successfully created post.',
     });
   } catch (e) {
     console.error(e);
+    response.statusCode = 500;
     response.body = JSON.stringify({
       message: 'Failed to create post.',
       errorMsg: e.message,
@@ -101,21 +90,15 @@ const createEmployee = async (event) => {
   return response;
 };
 
-// Function to update an employee
 const updateEmployee = async (event) => {
   const response = { statusCode: 200 };
   try {
-    // Parse the JSON body from the event
     const body = JSON.parse(event.body);
     const objKeys = Object.keys(body);
-    
-    // Perform validation on bankDetails
-    const validationError = validation(body.bankDetails);
+    const validationError = validation(body);
     if (validationError) {
       throw new Error(validationError);
     }
-
-    // Define parameters for updating an item in DynamoDB
     const params = {
       TableName: process.env.DYNAMODB_TABLE_NAME,
       Key: marshall({ postId: event.pathParameters.postId }),
@@ -139,17 +122,16 @@ const updateEmployee = async (event) => {
         )
       ),
     };
-
-    // Update the item in DynamoDB
     const updateResult = await db.send(new UpdateItemCommand(params));
     response.body = JSON.stringify({
-      message: 'Successfully updated BankDetails.',
+      message: 'Successfully updated post.',
       updateResult,
     });
   } catch (e) {
     console.error(e);
+    response.statusCode = 500;
     response.body = JSON.stringify({
-      message: 'Failed to update BankDetails.',
+      message: 'Failed to update post.',
       errorMsg: e.message,
       errorStack: e.stack,
     });
@@ -157,7 +139,6 @@ const updateEmployee = async (event) => {
   return response;
 };
 
-// Export the createEmployee and updateEmployee functions
 module.exports = {
   createEmployee,
   updateEmployee
--- test.js
+++ test.js
@@ -12,21 +12,8 @@ const mockClient = {
 
 // Mock employee data for createEmployee
 const createEmployeeData = {
-    postId: "25",
-    bankDetails: {
-      BankName: "kanara",
-      BranchName: "hydrabad",
-      BranchAddress: "bangalore",
-      CustomerNumber: "12345678912",
-      BankAccountNumber: "55566444412",
-      IsSalaryAccount: "yes",
-      IsActive: "yes",
-      IsDeleted: "false"
-    }
-  };
-
-// Mock employee data for updateEmployee
-const updateEmployeeData = {
+  postId: "25",
+  bankDetails: {
     BankName: "kanara",
     BranchName: "hydrabad",
     BranchAddress: "bangalore",
@@ -34,102 +21,112 @@ const updateEmployeeData = {
     BankAccountNumber: "55566444412",
     IsSalaryAccount: "yes",
     IsActive: "yes",
-    IsDeleted: "flase"
+    IsDeleted: "false"
+  }
+};
+
+// Mock employee data for updateEmployee
+const updateEmployeeData = {
+  BankName: "kanara",
+  BranchName: "hydrabad",
+  BranchAddress: "bangalore",
+  CustomerNumber: "12345678912",
+  BankAccountNumber: "55566444412",
+  IsSalaryAccount: "yes",
+  IsActive: "yes",
+  IsDeleted: "flase"
 };
 
 // Successfully create an employee
-// Successfully create an employee
 describe('createEmployee unit tests', () => {
-    let originalDynamoDBClient;
-  
-    before(() => {
-      originalDynamoDBClient = DynamoDBClient;
-      DynamoDBClient.prototype.send = () => mockClient.send();
-    });
-  
-    after(() => {
-      DynamoDBClient.prototype.send = originalDynamoDBClient.prototype.send;
-    });
-  
-    it('successfully create an employee', async () => {
-      // Mock event object with employee data
-      let event = {
-        body: JSON.stringify(createEmployeeData),
-      };
-    
-      const response = await createEmployee(event);
-      expect(response.statusCode).to.equal(200);
-      const responseBody = JSON.parse(response.body);
-      expect(responseBody.message).to.equal('Successfully created post.');
-    });
-    
-    it('fails to create an employee with missing data', async () => {
-      // Mock event object with missing data
-      let event = {
-        body: JSON.stringify({}), // Missing required data
-      };
-    
-      const response = await createEmployee(event);
-      expect(response.statusCode).to.equal(400); // Expecting a 400 Bad Request for missing data
-    });
-    
-    it('fails to create an employee with invalid data', async () => {
-      // Mock event object with invalid data
-      let event = {
-        body: JSON.stringify({
-          // Invalid data that should fail validation
-          BankName: 'AB', // Too short
-        }),
-      };
-    
-      const response = await createEmployee(event);
-      expect(response.statusCode).to.equal(400); // Expecting a 400 Bad Request for invalid data
-    });
-    
-  
-  // Successfully update an employee
-  describe('updateEmployee unit tests', () => {
-    let originalDynamoDBClient;
-  
-    before(() => {
-      originalDynamoDBClient = DynamoDBClient;
-      DynamoDBClient.prototype.send = () => mockClient.send();
-    });
-  
-    after(() => {
-      DynamoDBClient.prototype.send = originalDynamoDBClient.prototype.send;
-    });
-  
-    it('successfully update an employee', async () => {
-      // Mock event object with the employee ID and updated data
-      let event = {
-        pathParameters: {
-          postId: '25', // Assuming this postId exists
-        },
-        body: JSON.stringify(updateEmployeeData),
-      };
-    
-      const response = await updateEmployee(event);
-      expect(response.statusCode).to.equal(200);
-      const responseBody = JSON.parse(response.body);
-      expect(responseBody.message).to.equal('Successfully updated BankDetails.'); // Update the message if necessary
-    });
-    
-    it('fails to update an employee with invalid data', async () => {
-      // Mock event object with invalid data
-      let event = {
-        pathParameters: {
-          postId: '25', // Assuming this postId exists
-        },
-        body: JSON.stringify({
-          // Invalid data that should fail validation
-          BankName: 'a', // Too short
-        }),
-      };
-    
-      const response = await updateEmployee(event);
-      expect(response.statusCode).to.equal(400); // Expecting a 400 Bad Request for invalid data
-    });
-    
+  let originalDynamoDBClient;
+
+  before(() => {
+    originalDynamoDBClient = DynamoDBClient;
+    DynamoDBClient.prototype.send = () => mockClient.send();
   });
-  
+
+  after(() => {
+    DynamoDBClient.prototype.send = originalDynamoDBClient.prototype.send;
+  });
+
+  it('successfully create an employee', async () => {
+    // Mock event object with employee data
+    let event = {
+      body: JSON.stringify(createEmployeeData),
+    };
+
+    const response = await createEmployee(event);
+    expect(response.statusCode).to.equal(200);
+    const responseBody = JSON.parse(response.body);
+    expect(responseBody.message).to.equal('Successfully created post.');
+  });
+
+  it('fails to create an employee with missing data', async () => {
+    // Mock event object with missing data
+    let event = {
+      body: JSON.stringify({}), // Missing required data
+    };
+
+    const response = await createEmployee(event);
+    expect(response.statusCode).to.equal(400); // Expecting a 400 Bad Request for missing data
+  });
+
+  it('fails to create an employee with invalid data', async () => {
+    // Mock event object with invalid data
+    let event = {
+      body: JSON.stringify({
+        // Invalid data that should fail validation
+        BankName: 'AB', // Too short
+      }),
+    };
+
+    const response = await createEmployee(event);
+    expect(response.statusCode).to.equal(400); // Expecting a 400 Bad Request for invalid data
+  });
+});
+
+// Successfully update an employee
+describe('updateEmployee unit tests', () => {
+  let originalDynamoDBClient;
+
+  before(() => {
+    originalDynamoDBClient = DynamoDBClient;
+    DynamoDBClient.prototype.send = () => mockClient.send();
+  });
+
+  after(() => {
+    DynamoDBClient.prototype.send = originalDynamoDBClient.prototype.send;
+  });
+
+  it('successfully update an employee', async () => {
+    // Mock event object with the employee ID and updated data
+    let event = {
+      pathParameters: {
+        postId: '25', // Assuming this postId exists
+      },
+      body: JSON.stringify(updateEmployeeData),
+    };
+
+    const response = await updateEmployee(event);
+    expect(response.statusCode).to.equal(200);
+    const responseBody = JSON.parse(response.body);
+    expect(responseBody.message).to.equal('Successfully updated BankDetails.'); // Update the message if necessary
+  });
+
+  it('fails to update an employee with invalid data', async () => {
+    // Mock event object with invalid data
+    let event = {
+      pathParameters: {
+        postId: '25', // Assuming this postId exists
+      },
+      body: JSON.stringify({
+        // Invalid data that should fail validation
+        BankName: 'a', // Too short
+      }),
+    };
+
+    const response = await updateEmployee(event);
+    expect(response.statusCode).to.equal(400); // Expecting a 400 Bad Request for invalid data
+  });
+});
