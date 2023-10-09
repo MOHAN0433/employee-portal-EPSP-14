a6eb84031bbfe40b682a45f760520db1a7e7d7a8
Mohan Patil <mohan.patil@hyniva.com>
2023-10-09 17:15:01 +0530

    working new changes

--- api.js
+++ api.js
@@ -1,34 +1,47 @@
 // Import necessary modules from AWS SDK for DynamoDB
-const { DynamoDBClient, PutItemCommand, GetItemCommand, UpdateItemCommand } = require("@aws-sdk/client-dynamodb");
+const {
+  DynamoDBClient,
+  PutItemCommand,
+  GetItemCommand,
+  UpdateItemCommand,
+} = require("@aws-sdk/client-dynamodb");
 const { marshall, unmarshall } = require("@aws-sdk/util-dynamodb");
 
 // Create a DynamoDB client for the specified AWS region
 const db = new DynamoDBClient({ region: "ap-south-1" });
 
 // Define regular expressions for validation
+//validatins for name
 const nameRegex = /^[A-Za-z]{3,32}$/;
-const CustomerNumberRegex = /^\d{11,12}$/;
+//The customerNumber should be 11 digits only
+const CustomerNumberRegex = /^\d{11}$/;
+//BankAccountNumber should be minimum 11 and max 16 digits
 const BankAccountNumber = /^\d{11,16}$/;
 
 // Validation function for bankDetails object
 const validation = (bankDetails) => {
+  //bankname validation for minimum 3 characters
   if (!nameRegex.test(bankDetails.BankName)) {
     return "BankName should be minimum 3 characters!";
   }
+  //branchName should be minimum 3 characters
   if (!nameRegex.test(bankDetails.BranchName)) {
     return "BranchName should be minimum 3 characters!";
   }
+  //branchAddress should be minimum 3 characters
   if (!nameRegex.test(bankDetails.BranchAddress)) {
     return "BranchAddress should be minimum 3 characters!";
   }
+  //customerNumber should be 11 characters
   if (!CustomerNumberRegex.test(bankDetails.CustomerNumber)) {
-    return "CustomerNumber should be minimum 11 characters!";
+    return "CustomerNumber should be 11 characters!";
   }
+  //bankDetails must be 11 digits
   if (!BankAccountNumber.test(bankDetails.BankAccountNumber)) {
     return "BankAccountNumber should be minimum 11 digits!";
   }
-  //return null; // Validation passed
-}
+  return null; // Validation passed
+};
 
 // Function to create an employee
 const createEmployee = async (event) => {
@@ -38,30 +51,38 @@ const createEmployee = async (event) => {
     const body = JSON.parse(event.body);
     const bankDetails = body.bankDetails;
     console.log(bankDetails);
-    
+
     // Perform validation on bankDetails
     const validationError = validation(bankDetails);
     if (validationError) {
-       response.statusCode =400;
-      response.body=JSON.stringify({
+      response.statusCode = 400;
+      response.body = JSON.stringify({
         message: validationError,
-      })
+      });
       throw new Error(validationError);
     }
 
     //Check for required fields in the body
-    const requiredFields = ['BankName', 'BranchName','BranchAddress', 'CustomerNumber', 'BankAccountNumber', 'IsSalaryAccount', 'IsActive', 'IsDeleted'];
-    for (const field of requiredFields) {
+    const requiredBankDetails = [
+      "BankName",
+      "BranchName",
+      "BranchAddress",
+      "CustomerNumber",
+      "BankAccountNumber",
+      "IsSalaryAccount",
+      "IsActive",
+      "IsDeleted",
+    ];
+    for (const field of requiredBankDetails) {
       if (!body.bankDetails[field]) {
         response.statusCode = 400;
         throw new Error(`${field} is a mandatory field!`);
       }
     }
-    if(!body.postId) {
-      response.statusCode=400;
-      throw new Error('postId is a mandatory field!');
+    if (!body.postId) {
+      response.statusCode = 400;
+      throw new Error("postId is a mandatory field!");
     }
-    
 
     // Fetch an item from DynamoDB based on postId
     const employeeData = {
@@ -69,7 +90,7 @@ const createEmployee = async (event) => {
       Key: marshall({ postId: body.postId }),
     };
     const { Item } = await db.send(new GetItemCommand(employeeData));
-    
+
     // Check if an item with the same postId exists in DynamoDB
     if (Item) {
       const item1 = { item2: Item ? unmarshall(Item) : {} };
@@ -97,12 +118,12 @@ const createEmployee = async (event) => {
           IsDeleted: bankDetails.IsDeleted,
         },
       }),
-          };
+    };
 
     // Insert the item into DynamoDB
     await db.send(new PutItemCommand(params));
     response.body = JSON.stringify({
-      message: 'Successfully created BankDetails!',
+      message: "Successfully created BankDetails!",
     });
     //To through the exception if anything failing while creating bankDetails
   } catch (e) {
@@ -124,14 +145,14 @@ const updateEmployee = async (event) => {
     // Parse the JSON body from the event
     const body = JSON.parse(event.body);
     const objKeys = Object.keys(body);
-    
+
     // Perform validation on bankDetails
     const validationError = validation(body.bankDetails);
     if (validationError) {
-       response.statusCode =400;
-      response.body=JSON.stringify({
+      response.statusCode = 400;
+      response.body = JSON.stringify({
         message: validationError,
-      })
+      });
       throw new Error(validationError);
     }
 
@@ -141,7 +162,7 @@ const updateEmployee = async (event) => {
       Key: marshall({ postId: event.pathParameters.postId }),
       UpdateExpression: `SET ${objKeys
         .map((_, index) => `#key${index} = :value${index}`)
-        .join(', ')}`,
+        .join(", ")}`,
       ExpressionAttributeNames: objKeys.reduce(
         (acc, key, index) => ({
           ...acc,
@@ -163,13 +184,13 @@ const updateEmployee = async (event) => {
     // Update the item in DynamoDB
     const updateResult = await db.send(new UpdateItemCommand(params));
     response.body = JSON.stringify({
-      message: 'Successfully updated BankDetails.',
+      message: "Successfully updated BankDetails.",
       updateResult,
     });
   } catch (e) {
     console.error(e);
     response.body = JSON.stringify({
-      message: 'Failed to update BankDetails.',
+      message: "Failed to update BankDetails.",
       errorMsg: e.message,
       errorStack: e.stack,
     });
@@ -180,5 +201,5 @@ const updateEmployee = async (event) => {
 // Export the createEmployee and updateEmployee functions
 module.exports = {
   createEmployee,
-  updateEmployee
+  updateEmployee,
 };
